84455fae521d6ceda60663df1b4c3b39088ae293
JerrelMitchell <jerrelmitchell@ymail.com>
2018-05-10 13:20:30 -0600

    add test for create link on index page. add functionality in view for link to show

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'user sees all articles' do
   describe 'they visit /articles' do
     it 'displays all articles' do
+      create_link = 'Create A New Article'
       article1 = Article.create!(title: 'Title 1', body: 'Body 1')
       article2 = Article.create!(title: 'Title 2', body: 'Body 2')
 
@@ -10,6 +11,7 @@ describe 'user sees all articles' do
 
       expect(page).to have_link(article1.title)
       expect(page).to have_link(article2.title)
+      expect(page).to have_link(create_link)
     end
   end
 end
